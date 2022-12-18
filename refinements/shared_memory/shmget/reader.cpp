cb6b4afc2ea06e2ba7bc0b93768b2b5ffa01b3ba
bsobocki <bartq98@gmail.com>
2022-12-18 16:58:03 +0100

    fix problem with clearing shared memory in shmget refinement

--- refinements/shared_memory/shmget/reader.cpp
+++ refinements/shared_memory/shmget/reader.cpp
@@ -6,12 +6,13 @@
 // path to the file that will be used for generate a key for the shared memory
 //const auto keyFile = std::string("/tmp/msglib.key");
 constexpr key_t key = 0xc0de;
+constexpr auto size = 1024;
 
 int main() {
    //key_t key = ftok(keyFile.c_str(), 1);
 
    // create the shared memory
-   int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+   int shmid = shmget(key, size, 0666 | IPC_CREAT);
 
    // attach to the shared memory
    char *str = (char *)shmat(shmid, (void *)0, 0);
@@ -19,7 +20,8 @@ int main() {
    while (strcmp(str, "exit") != 0) {
       if (strlen(str)) {
          std::cout<< "Received: " << str << std::endl;
-         memcpy(str, "\0", 1);
+         memset(str, 0, size);
+         std::cout<< "Clean message: ||" << str << "||" << std::endl;
       }
       usleep(1000000);
    }
